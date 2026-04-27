db6e77be058beae29ccc320bc9d339d6c007e0ad
Harshani <harshani.wijaya@gmail.com>
2026-04-27 23:30:14 +0530

    feat: initialize express server and configure food routes with multer image upload middleware

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -1,6 +1,6 @@
-import express from "express";
-import { addFood } from "../controllers/foodController.js";
-import multer from "multer";
+import express from "express"
+import { addFood } from "../controllers/foodController.js"
+import multer from "multer"
 
 const foodRouter = express.Router();
 
@@ -8,13 +8,13 @@ const foodRouter = express.Router();
 
 const storage = multer.diskStorage({
   destination: "uploads",
-  filename:(req,file,cb)=>{
-    return cb(null,`${Date.now()}${file.originalname}`)
+  filename: (req, file, cb) => {
+    return cb(null, `${Date.now()}${file.originalname}`)
   }
 })
 
-const upload = multer({storage:storage})
+const upload = multer({ storage: storage })
 
-foodRouter.post("/add",upload.single("image"),addFood)
+foodRouter.post("/add", upload.single("image"), addFood)
 
 export default foodRouter;
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
-import express from "express";
-import cors from "cors";
-import { connectDB } from "./config/db.js";
-import foodRouter from "./routes/foodRoute.js";
+import express from "express"
+import cors from "cors"
+import { connectDB } from "./config/db.js"
+import foodRouter from "./routes/foodRoute.js"
 
 
 
@@ -19,11 +19,11 @@ connectDB();
 //api endpoints
 app.use("/api/food", foodRouter)
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
   res.send("API Working")
 })
 
-app.listen(port,()=>{
+app.listen(port, () => {
   console.log(`Server started on http://localhost:${port}`)
 })
 
